889f031f2f095b7d7349bdd49aae9f9a052a578d
4e1ik <Artemi324@tut.by>
2022-08-03 22:45:17 +0300

    init(03.08.2022): fix slider and buttons

--- src/script.js
+++ src/script.js
@@ -1,39 +1,62 @@
-/* Индекс слайда по умолчанию */
-var slideIndex = 1;
-showSlides(slideIndex);
+// /* Индекс слайда по умолчанию */
+// var slideIndex = 1;
+// showSlides(slideIndex);
 
-/* Функция увеличивает индекс на 1, показывает следующй слайд*/
-function plusSlide() {
-    showSlides(slideIndex += 1);
-}
+// /* Функция увеличивает индекс на 1, показывает следующй слайд*/
+// function plusSlide() {
+//     showSlides(slideIndex += 1);
+// }
 
-/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
-function minusSlide() {
-    showSlides(slideIndex -= 1);  
-}
+// /* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
+// function minusSlide() {
+//     showSlides(slideIndex -= 1);  
+// }
 
-/* Устанавливает текущий слайд */
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
+// /* Устанавливает текущий слайд */
+// function currentSlide(n) {
+//     showSlides(slideIndex = n);
+// }
 
-/* Основная функция сладера */
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("item");
-    var dots = document.getElementsByClassName("slider-dots_item");
-    if (n > slides.length) {
-      slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
+// /* Основная функция сладера */
+// function showSlides(n) {
+//     var i;
+//     var slides = document.getElementsByClassName("item");
+//     var dots = document.getElementsByClassName("slider-dots_item");
+//     if (n > slides.length) {
+//       slideIndex = 1
+//     }
+//     if (n < 1) {
+//         slideIndex = slides.length
+//     }
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     for (i = 0; i < dots.length; i++) {
+//         dots[i].className = dots[i].className.replace(" active", "");
+//     }
+//     slides[slideIndex - 1].style.display = "block";
+//     dots[slideIndex - 1].className += " active";
+// }
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+});
